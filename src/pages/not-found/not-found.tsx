e6b768f7e278b3d453a2586b5a6cd51a908d1955
AdonaiJehosua <adonaijehosua@gmail.com>
2024-04-02 21:47:28 +0500

    Опишет маршруты, добавит компонент приватных маршрутов и страницу ошибки 404

---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import './not-found.css';
+
+export default function NotFound(): JSX.Element {
+  return (
+    <div className="not-found-container">
+      <div className="not-found-information">
+        <div className='information--text error'>404</div>
+        <div className='information--text'>We really searched, but didn&apos;t find this page</div>
+        <Link className='button form__submit' to='/'>Home</Link>
+      </div>
+      <img className="not-found-image" src="img/not-found.webp" />
+    </div>
+  );
+}
