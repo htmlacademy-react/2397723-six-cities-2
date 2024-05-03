ffda5c9cdc39fbbcd5a50775d22c64164f5f020a
AdonaiJehosua <adonaijehosua@gmail.com>
2024-05-04 02:40:20 +0500

    Вынесет избранные предложения по городу в отдельный компонент, переместит функцию сортировки

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,10 @@
+import { CityName } from '../const/const';
+import { Offer } from '../types/offer';
+
 export function ratingToPercent(rating: number): string {
   return `${rating / 5 * 100}%`;
+}
+
+export function getCityOffers(cityName: CityName, favoriteOffers: Offer[]): Offer[] | undefined {
+  return favoriteOffers.filter((offer: Offer): boolean => offer.city.name === cityName);
 }
