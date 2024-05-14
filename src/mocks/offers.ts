974bae64a762c08988aa143d95f3fad14e525be0
AdonaiJehosua <adonaijehosua@gmail.com>
2024-05-14 07:29:47 +0500

    Добавит моковых данных

--- src/mocks/offers.ts
+++ src/mocks/offers.ts
@@ -25,7 +25,7 @@ export const offers: OfferData[] = [
     rating: 2.7
   },
   {
-    id: 'b312baee-786b-43bd-9fba-c19f0da74abc',
+    id: 'b312baee-78-9fba-c19f0da74abc',
     title: 'Waterfront with extraordinary view',
     type: 'room',
     price: 178,
@@ -48,7 +48,7 @@ export const offers: OfferData[] = [
     rating: 3.7
   },
   {
-    id: '40761631-ecb4-4f09-a7f9-610bd3b19689',
+    id: '40761631-4f09-a7f9-610bd3b19689',
     title: 'Beautiful & luxurious apartment at great location',
     type: 'house',
     price: 281,
@@ -71,13 +71,358 @@ export const offers: OfferData[] = [
     rating: 1.4
   },
   {
-    id: '31775939-9267-4776-b3b3-6a5d097c5871',
+    id: '31775939-4776-b3b3-6a5d097c5871',
     title: 'Tile House',
     type: 'hotel',
-    price: 339,
+    price: 726,
     previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
     city: {
       name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-6a5d097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 2457,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Brussels',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-b3b3-6871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 765,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Brussels',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-67-4776-b3b3-6a5d097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 632,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '317939-9267-4776-b3b3-6a5d097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 256,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Cologne',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-b3-6a5d097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 2256,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Cologne',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-926776-b3b3-6a5d097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 753,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-967-4776-b3b3-6a5d097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 345,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-b3b36a5d097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 4443,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-3b3-6a5d097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 65656,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Hamburg',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-476-b3b3-6a5d097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 11,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-b3b3-6a5d0975871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 222,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Hamburg',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-b3b3-6a5097c5871',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 4354,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Hamburg',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-b3b3-6a5d097c581',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 63,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Dusseldorf',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-b3b3-6a5d097c5851',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 624,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Dusseldorf',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.3809553943508,
+      longitude: 4.939309666406198,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.5
+  },
+  {
+    id: '31775939-9267-4776-b3b3-6a5d097c5878',
+    title: 'Tile House',
+    type: 'hotel',
+    price: 6245,
+    previewImage: 'https://13.design.htmlacademy.pro/static/hotel/1.jpg',
+    city: {
+      name: 'Dusseldorf',
       location: {
         latitude: 52.37454,
         longitude: 4.897976,
