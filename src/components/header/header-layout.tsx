3625581eae5d62982eaa2e0739997698195f8523
AdonaiJehosua <adonaijehosua@gmail.com>
2024-06-04 22:36:44 +0500

    Переработает отображение загрузки

--- src/components/header/header-layout.tsx
+++ src/components/header/header-layout.tsx
@@ -5,7 +5,28 @@ import { useAppSelector } from '../../hooks/redux-ts';
 import Spinner from '../spinner/spinner';
 
 export default function HeaderLayout(): React.JSX.Element {
-  const isLoading = useAppSelector((state) => state.OFFERS.isOffersLoading);
+  const isOffersLoading = useAppSelector((state) => state.OFFERS.isOffersLoading);
+  const isOfferLoading = useAppSelector((state) => state.OFFER.isOfferLoading);
+  const isNearPlacesLoading = useAppSelector((state) => state.NEAR_PLACES.isNearPlacesLoading);
+  const isFavoritesLoading = useAppSelector((state) => state.FAVORITES.isFavoritesLoading);
+  const isStatusLoading = useAppSelector((state) => state.FAVORITES.isStatusSanding);
+  const isReviewsLoading = useAppSelector((state) => state.REVIEWS.isReviewsLoading);
+  const isReviewsSanding = useAppSelector((state) => state.REVIEWS.isReviewSanding);
+  const isCheckAuthLoading = useAppSelector((state) => state.USER.isCheckAuthLoading);
+  const isLoginLoading = useAppSelector((state) => state.USER.isLoginLoading);
+  const isLogoutLoading = useAppSelector((state) => state.USER.isLogoutLoading);
+
+  const isLoading =
+    isOffersLoading
+    || isOfferLoading
+    || isNearPlacesLoading
+    || isFavoritesLoading
+    || isStatusLoading
+    || isReviewsLoading
+    || isReviewsSanding
+    || isCheckAuthLoading
+    || isLoginLoading
+    || isLogoutLoading;
 
   return (
     < div className="page page--gray page--main" >
@@ -17,7 +38,7 @@ export default function HeaderLayout(): React.JSX.Element {
                 {isLoading ? <Spinner /> : <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />}
               </Link>
             </div>
-            <HeaderNav />
+            {!isLoginLoading && !isCheckAuthLoading && <HeaderNav />}
           </div>
         </div>
       </header>
--- src/store/user-data/user-data.ts
+++ src/store/user-data/user-data.ts
@@ -6,11 +6,17 @@ import { checkAuth, login, logout } from '../api-actions';
 type UserData = {
   authorizationStatus: AuthorizationStatus;
   user: User | null;
+  isCheckAuthLoading: boolean;
+  isLoginLoading: boolean;
+  isLogoutLoading: boolean;
 };
 
 const initialState: UserData = {
   authorizationStatus: AuthorizationStatus.Unknown,
-  user: null
+  user: null,
+  isCheckAuthLoading: false,
+  isLoginLoading: false,
+  isLogoutLoading: false,
 };
 
 export const userData = createSlice({
@@ -19,25 +25,42 @@ export const userData = createSlice({
   reducers: {},
   extraReducers(builder) {
     builder
+      .addCase(checkAuth.pending, (state) => {
+        state.isCheckAuthLoading = true;
+      })
       .addCase(checkAuth.fulfilled, (state, action) => {
         state.authorizationStatus = AuthorizationStatus.Auth;
         state.user = action.payload;
+        state.isCheckAuthLoading = false;
       })
       .addCase(checkAuth.rejected, (state) => {
         state.authorizationStatus = AuthorizationStatus.NoAuth;
         state.user = null;
+        state.isCheckAuthLoading = false;
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoginLoading = true;
       })
       .addCase(login.fulfilled, (state, action) => {
         state.authorizationStatus = AuthorizationStatus.Auth;
         state.user = action.payload;
+        state.isLoginLoading = false;
       })
       .addCase(login.rejected, (state) => {
         state.authorizationStatus = AuthorizationStatus.NoAuth;
         state.user = null;
+        state.isLoginLoading = false;
+      })
+      .addCase(logout.pending, (state) => {
+        state.isLogoutLoading = true;
       })
       .addCase(logout.fulfilled, (state) => {
         state.authorizationStatus = AuthorizationStatus.NoAuth;
         state.user = null;
+        state.isLogoutLoading = false;
+      })
+      .addCase(logout.rejected, (state) => {
+        state.isLogoutLoading = false;
       });
   }
 });
