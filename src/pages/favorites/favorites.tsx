be30589f01f2fcd51892a915ee713ca594996640
AdonaiJehosua <adonaijehosua@gmail.com>
2024-06-04 19:07:04 +0500

    Доработает страницу Favorites при условии отсутствия избранных предложений

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,8 +1,43 @@
 import React, { useEffect } from 'react';
 import FavoritesList from '../../components/favorites-list/favorites-list';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux-ts';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { fetchFavorites } from '../../store/api-actions';
+import { AppRoute } from '../../const/const';
+import { OfferData } from '../../types/offer';
+
+type NotEmptyFavoritesProps = {
+  favorites: OfferData[];
+}
+
+function NotEmptyFavorites({ favorites }: NotEmptyFavoritesProps): React.JSX.Element {
+  return (
+    <main className="page__main page__main--favorites">
+      <div className="page__favorites-container container">
+        <section className="favorites">
+          <h1 className="favorites__title">Saved listing</h1>
+          <FavoritesList favoriteOffers={favorites} />
+        </section>
+      </div>
+    </main>
+  );
+}
+
+function EmptyFavorites(): React.JSX.Element {
+  return (
+    <main className="page__main page__main--favorites page__main--favorites-empty">
+      <div className="page__favorites-container container">
+        <section className="favorites favorites--empty">
+          <h1 className="visually-hidden">Favorites (empty)</h1>
+          <div className="favorites__status-wrapper">
+            <b className="favorites__status">Nothing yet saved.</b>
+            <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
 
 export default function Favorites(): React.JSX.Element {
   const dispatch = useAppDispatch();
@@ -14,20 +49,13 @@ export default function Favorites(): React.JSX.Element {
   const favorites = useAppSelector((state) => state.FAVORITES.favorites);
 
   return (
-    <div className="page">
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <FavoritesList favoriteOffers={favorites} />
-          </section>
-        </div>
-      </main>
+    <>
+      {favorites.length > 0 ? <NotEmptyFavorites favorites={favorites} /> : <EmptyFavorites />}
       <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
+        <Link className="footer__logo-link" to={AppRoute.Main}>
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-        </a>
+        </Link>
       </footer>
-    </div>
+    </>
   );
 }
